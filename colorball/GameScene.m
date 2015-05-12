7ba20eded3a36d0a87abf86d223c36261e455edb
nvsfs <nataliafrs@Natalias-MacBook-Pro-3.local>
2015-05-12 12:09:13 -0300

    first commit

--- colorball/GameScene.m
+++ colorball/GameScene.m
@@ -7,40 +7,103 @@
 //
 
 #import "GameScene.h"
+#define colunas 6
+#define linhas 8
 
 @implementation GameScene
 
+
+-(void)Blockfall: (CGSize)size withPosition:(CGPoint)position andColor :(UIColor*)cor{
+    
+    
+    SKSpriteNode *ball = [SKSpriteNode spriteNodeWithColor:cor size:size];
+    
+    ball.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:CGSizeMake(size.width-2, size.height-2)];
+    
+    ball.position = position;
+    
+    ball.physicsBody.restitution = 0.f;
+    
+    ball.physicsBody.allowsRotation = false;
+    
+    [self addChild:ball];
+    
+
+}
+
+-(id)initWithSize:(CGSize)size{
+
+    if (self = [super initWithSize:size]){
+        
+        self.backgroundColor = [SKColor colorWithRed:0.15 green:0.4 blue:0.3 alpha:1.0];
+        
+        self.physicsWorld.gravity = CGVectorMake(0, -8.f);
+        
+//    SKSpriteNode *ball = [SKSpriteNode spriteNodeWithImageNamed:@"Image.png"];
+//    
+//    ball.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:ball.size];
+//    
+//    ball.position = CGPointMake(160, 480);
+//        
+//    ball.physicsBody.restitution = 0.f;
+//        
+//    [self addChild:ball];
+        
+    SKSpriteNode *chao = [SKSpriteNode spriteNodeWithColor:[UIColor blackColor] size:CGSizeMake(640, 80)];
+        
+        chao.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:chao.size];
+        
+        chao.physicsBody.dynamic = false;
+        
+        chao.position = CGPointMake(160, 20);
+        
+        chao.physicsBody.mass = -1;
+        
+        [self addChild:chao];
+        
+        
+        for (int linha= 0; linha < linhas; linha++ ){
+                for (int coluna= 0; coluna < colunas; coluna++ ){
+                    
+                    CGFloat dimension = self.scene.size.width / colunas;
+            CGFloat xposicao = (dimension / 2) + coluna * dimension;
+            CGFloat yposicao = 640 + ( (dimension/2) - linha * dimension );
+                    
+            NSArray *cores = @[[UIColor greenColor], [UIColor redColor], [UIColor blueColor], [UIColor yellowColor], [UIColor blackColor]];
+            
+            NSUInteger colorIndex = arc4random() % cores.count;
+            
+            [self Blockfall:CGSizeMake(dimension, dimension)
+               withPosition:CGPointMake(xposicao, yposicao)
+                   andColor:[cores objectAtIndex:colorIndex]];
+            }
+        }
+    
+    }
+
+    return self;
+
+}
 -(void)didMoveToView:(SKView *)view {
     /* Setup your scene here */
-    SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
-    
-    myLabel.text = @"Hello, World!";
-    myLabel.fontSize = 65;
-    myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                   CGRectGetMidY(self.frame));
-    
-    [self addChild:myLabel];
+//
+//    self.physicsWorld.gravity = CGVectorMake(0, 1.0f);
+//    
+//    SKSpriteNode *ball = [SKSpriteNode spriteNodeWithColor: [UIColor redColor] size:CGSizeMake(100, 100)];
+//    
+//    ball.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:ball.size];
+//    
+//    ball.position = CGPointMake(160, 480);
+//    
+//    [self addChild:ball];
+
+
 }
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     /* Called when a touch begins */
-    
-    for (UITouch *touch in touches) {
-        CGPoint location = [touch locationInNode:self];
-        
-        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
-        
-        sprite.xScale = 0.5;
-        sprite.yScale = 0.5;
-        sprite.position = location;
-        
-        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
-        
-        [sprite runAction:[SKAction repeatActionForever:action]];
-        
-        [self addChild:sprite];
-    }
 }
+
 
 -(void)update:(CFTimeInterval)currentTime {
     /* Called before each frame is rendered */
--- colorball/GameViewController.m
+++ colorball/GameViewController.m
@@ -42,13 +42,29 @@
     skView.ignoresSiblingOrder = YES;
     
     // Create and configure the scene.
-    GameScene *scene = [GameScene unarchiveFromFile:@"GameScene"];
+    GameScene *scene = [GameScene sceneWithSize:skView.bounds.size];
     scene.scaleMode = SKSceneScaleModeAspectFill;
     
     // Present the scene.
     [skView presentScene:scene];
 }
 
+//{
+//    [super viewWillLayoutSubviews];
+//    
+//    // Configure the view.
+//    SKView * skView = (SKView *)self.view;
+//    if (!skView.scene) {
+//        skView.showsFPS = YES;
+//        skView.showsNodeCount = YES;
+//        
+//        // Create and configure the scene.
+//        SKScene * scene = [MyScene sceneWithSize:skView.bounds.size];
+//        scene.scaleMode = SKSceneScaleModeAspectFill;
+//        
+//        // Present the scene.
+//        [skView presentScene:scene];
+//    }
 - (BOOL)shouldAutorotate
 {
     return YES;
